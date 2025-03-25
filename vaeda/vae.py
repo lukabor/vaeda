fe4e9ec0d470a45e301a5fb024cf82562a57b761
lukabor <94577648+lukabor@users.noreply.github.com>
2025-03-25 09:24:30 +0100

    fix(vae): adapt to tensorflow and keras > 2.15
    fix(define_clust_vae): adapt lambda wrapper for IndependentNormal for tf > 2.15
    fix(define_vae): adapt lambda wraper for IndependentNormal for tf > 2.15

--- vaeda/vae.py
+++ vaeda/vae.py
@@ -1,4 +1,3 @@
-#- vae
 import tensorflow as tf
 import tensorflow_probability as tfp
 import numpy as np
@@ -9,7 +8,6 @@ def define_clust_vae(enc_sze, ngens, num_clust, LR=1e-3, clust_weight=10000):
     tfkl = tf.keras.layers
     tfpl = tfp.layers
     tfd  = tfp.distributions
-
     prior = tfd.Independent(tfd.Normal(loc=tf.zeros(enc_sze), scale=1),
             reinterpreted_batch_ndims=1)
     
@@ -19,21 +17,21 @@ def define_clust_vae(enc_sze, ngens, num_clust, LR=1e-3, clust_weight=10000):
         tfkl.BatchNormalization(),
         tfkl.Dropout(rate=0.3),
         tfkl.Dense(tfpl.IndependentNormal.params_size(enc_sze), activation=None),
-        tfpl.IndependentNormal(
+        tfkl.Lambda(lambda x: tfpl.IndependentNormal(
             enc_sze,
             activity_regularizer=tfpl.KLDivergenceRegularizer(prior)
-        )
+        )(x))
     ], name='encoder')
-
+    
     decoder = tfk.Sequential([
         tfkl.InputLayer(input_shape=[enc_sze]),
         tfkl.Dense(256, activation='relu'),
         tfkl.BatchNormalization(),
         tfkl.Dropout(rate=0.3),
         tfkl.Dense(tfpl.IndependentNormal.params_size(ngens), activation=None),
-        tfpl.IndependentNormal(ngens)
+        tfkl.Lambda(lambda x: tfpl.IndependentNormal(ngens)(x))
     ], name='decoder')
-
+    
     clust_classifier = tfk.Sequential([
         tfkl.InputLayer(input_shape=[enc_sze]),
         tfkl.BatchNormalization(),
@@ -44,7 +42,6 @@ def define_clust_vae(enc_sze, ngens, num_clust, LR=1e-3, clust_weight=10000):
     z       = encoder(IPT)
     OPT1    = decoder(z)
     OPT2    = clust_classifier(z)
-
     vae = tfk.Model(inputs=[IPT],
                       outputs=[OPT1, OPT2])
     
@@ -52,11 +49,11 @@ def define_clust_vae(enc_sze, ngens, num_clust, LR=1e-3, clust_weight=10000):
         rec = rv_x.log_prob(x)
         return -tf.math.reduce_sum(rec, axis=-1) 
     
-    vae.compile(optimizer = tf.optimizers.Adamax(learning_rate=LR),#Adam, 1e-3
-                  loss=[nll, 'categorical_crossentropy'], loss_weights=[1,clust_weight])
+    vae.compile(optimizer = tf.optimizers.Adamax(learning_rate=LR),
+                loss=[nll, 'categorical_crossentropy'], 
+                loss_weights=[1, clust_weight])
   
     return vae
-
     
 def define_vae(enc_sze, ngens):
     
@@ -64,7 +61,6 @@ def define_vae(enc_sze, ngens):
     tfkl = tf.keras.layers
     tfpl = tfp.layers
     tfd  = tfp.distributions
-
     prior = tfd.Independent(tfd.Normal(loc=tf.zeros(enc_sze), scale=1),
             reinterpreted_batch_ndims=1)
     
@@ -74,40 +70,32 @@ def define_vae(enc_sze, ngens):
         tfkl.BatchNormalization(),
         tfkl.Dropout(rate=0.3),
         tfkl.Dense(tfpl.IndependentNormal.params_size(enc_sze), activation=None),
-        tfpl.IndependentNormal(
+        tfkl.Lambda(lambda x: tfpl.IndependentNormal(
             enc_sze,
             activity_regularizer=tfpl.KLDivergenceRegularizer(prior)
-        )
+        )(x))
     ], name='encoder')
-
+    
     decoder = tfk.Sequential([
         tfkl.InputLayer(input_shape=[enc_sze]),
         tfkl.Dense(256, activation='relu'),
         tfkl.BatchNormalization(),
         tfkl.Dropout(rate=0.3),
         tfkl.Dense(tfpl.IndependentNormal.params_size(ngens), activation=None),
-        tfpl.IndependentNormal(ngens)
+        tfkl.Lambda(lambda x: tfpl.IndependentNormal(ngens)(x))
     ], name='decoder')
-
-
+    
     IPT     = tfk.Input(shape = ngens)
     z       = encoder(IPT)
     OPT1    = decoder(z)
-
     vae = tfk.Model(inputs=[IPT],
-                      outputs=[OPT1])
+                    outputs=[OPT1])
     
     def nll(x, rv_x): 
         rec = rv_x.log_prob(x)
         return -tf.math.reduce_sum(rec, axis=-1) 
     
-    vae.compile(optimizer = tf.optimizers.Adamax(learning_rate=1e-3),#Adam
-                  loss=nll)
-
+    vae.compile(optimizer = tf.optimizers.Adamax(learning_rate=1e-3),
+                loss=nll)
+    
     return vae
-
-
-
-    
-    
-    
